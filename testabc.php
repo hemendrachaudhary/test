22110ef07dc9b06742b9b82f3e1cc3c341e2c550
hemendrachaudhary <31954444+hemendrachaudhary@users.noreply.github.com>
2017-12-15 13:22:44 +0530

    Update testabc.php

--- testabc.php
+++ testabc.php
@@ -97,4 +97,112 @@ public function create_visitor($data) {
 			}
 		}
 
+//controller
+
+
+
+$this->data['title'] = 'Portfolio';
+$this->load->view('static/header');
+$this->form_validation->set_rules('productName','Product Name','trim|required');
+$this->form_validation->set_rules('productPrice','Product Price','trim|required');
+$this->form_validation->set_rules('productDiscount','Product Discount','trim|required');
+$this->form_validation->set_rules('description','Product Description','trim|required');
+//$this->form_validation->set_rules('userfile','Product Image','trim|required');
+if($this->form_validation->run()==FALSE){
+$this->load->view('create');
+}
+else
+{
+
+$upload_img = $this->cwUpload('image','./assets/images/gallery/','',TRUE,'./assets/images/gallery/thumb/','120','120');
+
+
+		//full path of the thumbnail image
+	        		// $thumb_src = './assets/images/gallery/thumb/'.$upload_img;
+
+		//set success and error messages
+	        		$message = $upload_img?"<span style='color:#008000;'>Image thumbnail have been created successfully.</span>":"<span style='color:#F00000;'>Some error occurred, please try again.</span>";
+
+	        		
+	        			// "contentID" => $this->visitorID,
+	        			
+	        			
+				$data = array('productID'=>'',
+					"imageName" => $upload_img,
+					'productName'=>$this->input->post("productName"),
+                    'productPrice'=>$this->input->post("productPrice"),
+                    'productDiscount'=>$this->input->post("productDiscount"),
+                    'productDescription' =>$this->input->post("description"),
+                    
+				);
+				$done = $this->My_model->insert('aproduct',$data);
+          if($done){
+          	redirect(base_url());
+          }
+
+			}
+
+$this->load->view('static/footer');
+}
+
+///view
+
+	
+    <section id="">
+	    <br><br>
+        <div class="container">
+            <div class="center">        
+                <h2>Add Product </h2>
+                <p class="lead">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div> 
+            <div class="row contact-wrap"> 
+                <div class="status alert alert-success" style="display: none"></div>
+                <form id="" class="" name="" method="post" enctype="multipart/form-data" action="<?php echo base_url(); ?>Auth/addProduct">
+                    <div class="col-sm-4 ">
+                        <div class="form-group">
+                            <label>Product Name *</label>
+                            <input type="text" name="productName" id="productName" class="form-control" >
+                          </div>
+                          <span class="err"><?php echo form_error('productName') ?></span>
+                    </div>
+                        <div class="col-sm-4 ">
+                        <div class="form-group">
+                            <label>Product Price </label>
+                            <input type="text" id="productPrice" name="productPrice" class="form-control" >
+                        </div>
+                        <span class="err"><?php echo form_error('productPrice') ?></span>
+                    </div>
+                        <div class="col-sm-4 ">
+                        <div class="form-group">
+                            <label>Product Discount </label>
+                            <input type="text" id="productDiscount" name="productDiscount" class="form-control" >
+                        </div>
+                        <span class="err"><?php echo form_error('productDiscount') ?></span>
+                    </div>
+                    <div class="col-sm-4 ">
+                        <div class="form-group">
+                            <label>File *</label>
+                            <input type="file" name="image" id="image" class="form-control" >
+                        </div>
+                          <!-- <span class="err"><?php echo form_error('userfile') ?></span> -->
+                    </div>
+                    <div class="col-sm-4 ">
+                        <div class="form-group">
+                            <label>Product Description *</label>
+                             <input type="text" name="description" id="description" class="form-control" >
+                          
+                        </div> 
+                        <span class="err"><?php echo form_error('description') ?></span>
+                        </div>
+
+                        <div class="form-group pull-right">
+                            
+                             <button type="submit" name="submit" class="btn btn-primary btn-lg" id="error">Add</button> 
+                        </div>
+                    </div>
+                </form> 
+            </div><!--/.row-->
+        </div><!--/.container-->
+    </section><!--/#contact-page-->
+
 
